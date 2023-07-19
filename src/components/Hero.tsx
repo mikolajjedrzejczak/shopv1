0179e869c56ceea3d0b071b3d8f07493adbf6612
MKJ1337 <mik.jed2003@gmail.com>
2023-07-19 12:12:19 +0200

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Hero = () => {
   return (
     <div className="w-full h-[55vh] overflow-hidden relative flex justify-center items-center fixed-img">
-      <div className="absolute flex flex-col items-center justify-between gap-10">
+      <div className="absolute flex flex-col items-center justify-between gap-10 px-[3rem] sm:px-0">
         <p className="text-white leading-[3rem] text-4xl sm:text-5xl text-center sm:leading-[4rem] font-thin">
           Introducing the Perfectly
           <br /> Simple Clothing Brand for
