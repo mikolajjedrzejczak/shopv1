1b0b37214dadbf9b2bbf634249f43706299eb32f
MKJ1337 <mik.jed2003@gmail.com>
2023-07-21 11:57:13 +0200

    update

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -6,15 +6,15 @@ const InfoBar = () => {
   return (
     <div className="w-full flex justify-center items-center bg-gray-100 py-10">
       <div className="w-[1200px] gap-10 md:gap-0 px-5 md:px-0 flex-col md:flex-row flex justify-between items-center text-gray-500">
-        <div className="bg-gray-200 px-[8rem] py-5 flex flex-col items-center">
+        <div className="bg-gray-200 px-[8rem] py-5 flex gap-5 md:gap-0 md:flex-col flex-row items-center">
           <LiaShippingFastSolid size={40} className="text-black" />
           <h2>Fast Shipping</h2>
         </div>
-        <div className="bg-gray-200 px-[8rem] py-5 flex flex-col items-center">
+        <div className="bg-gray-200 px-[8rem] py-5 flex gap-5 md:gap-0 md:flex-col flex-row items-center">
           <MdGppGood size={40} className="text-black" />
           <h2 className="font-semibold">High Quality</h2>
         </div>
-        <div className="bg-gray-200 px-[8rem] py-5 flex flex-col items-center">
+        <div className="bg-gray-200 px-[8rem] py-5 flex gap-5 md:gap-0 md:flex-col flex-row items-center">
           <GiPathDistance size={40} className="text-black" />
           <h2>Track Delivery</h2>
         </div>
